9243702692f59f9375731a1a4b3118f82e2b0f9e
iampurna <linkwithpurna@gmail.com>
2023-12-07 12:55:14 +0545

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+        child: const Column(
+          children: [
+            Text(
+              "Hello, Purna",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

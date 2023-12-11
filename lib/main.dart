d1039902ef95a2ebb950ecb577b005374242bcf5
iampurna <linkwithpurna@gmail.com>
2023-12-12 01:35:33 +0545

    ondoard screening

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/pages/login.dart';
+import 'package:food_delivery/pages/onboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LogIn(),
+      home: Onboard(),
     );
   }
 }
